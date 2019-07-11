d61f1245561549d22354e5d4b5392bdbdbd35ff7
zacharyeASC6 <zacharyetiennetbls@gmail.com>
2019-07-11 14:46:58 -0400

    For loop to multiply numbers in array

---
+++ gitPractice.js
@@ -0,0 +1,22 @@
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+
+function multiplyArrayNumbers(arr){
+    for (i = 0; i < arr.length; i++){
+        console.log("Multiplying element " + arr[i]);
+        arr[i] *= 2;
+        sleep(3000);
+        console.log("Doubled number: " + arr[i]);
+    }
+    console.log("Completed array: " + arr);
+}
+
+const numbersYay = [0.5,1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,];
+
+multiplyArrayNumbers(numbersYay);
